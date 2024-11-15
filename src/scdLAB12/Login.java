50390ab9f45c2592719d748285fe71559e2f3e7d
Asfand Ahmed <mhussnainzardari34@gmail.com>
2024-11-15 18:08:53 +0500

    Added basic Login functionality

---
+++ src/scdLAB12/Login.java
@@ -0,0 +1,8 @@
+package scdLAB12;
+
+public class Login {
+    public boolean authenticate(String username, String password) {
+        return username.equals("admin") && password.equals("password123");
+    }
+}
+
