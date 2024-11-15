7eb025b6fed63b60a0712499553be2be0655248b
Asjad <168066955+Mr-Asjad@users.noreply.github.com>
2024-11-15 19:01:49 +0500

    Changes in Main branch

--- src/scdLAB12/Main.java
+++ src/scdLAB12/Main.java
@@ -1,12 +1,11 @@
 package scdLAB12;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the application!");
-		//greetUser();
-	}
-
-	public static void greetUser() {
-		System.out.println("Hello, User!");
+public class Main 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Main branch update.");
+		
 	}
 }
