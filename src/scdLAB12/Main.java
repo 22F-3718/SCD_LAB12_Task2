8d240f24009d87bfe4adddabf96948942436dd45
Asjad <168066955+Mr-Asjad@users.noreply.github.com>
2024-11-15 11:11:52 +0500

    Update Main.java

--- src/scdLAB12/Main.java
+++ src/scdLAB12/Main.java
@@ -3,7 +3,7 @@ package scdLAB12;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		//greetUser();
+		//greetUser();                                              //Commented this Line
 	}
 
 	public static void greetUser() {
