0838c5b80afd2b8592f74d527d353d235e7a75bc
Muhammad Hasnain <mhussnainzardari34@gmail.com>
2024-11-15 18:48:56 +0500

    Feature A: Initial work in progress

--- src/scdLAB12/Main.java
+++ src/scdLAB12/Main.java
@@ -1,11 +1,12 @@
 package scdLAB12;
 
-public class Main 
-{
-	
-	public static void main(String[] args) 
-	{
-		System.out.println("Main branch update.");
-		
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the application!");
+		//greetUser();
+	}
+
+	public static void greetUser() {
+		System.out.println("Hello, User!");
 	}
 }
