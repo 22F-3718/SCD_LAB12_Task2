c8b785b5b386772cf0b41160476db7aaa9711cb2
Asfand Ahmed <mhussnainzardari34@gmail.com>
2024-11-15 18:10:56 +0500

    Add logging to authenticate method

--- src/scdLAB12/Login.java
+++ src/scdLAB12/Login.java
@@ -2,7 +2,9 @@ package scdLAB12;
 
 public class Login {
     public boolean authenticate(String username, String password) {
+        System.out.println("Authenticating user...");
         return username.equals("admin") && password.equals("password123");
     }
 }
+
 
